cfb9077663bb2a6cf47dc9646ca3bb61b16da7ed
monge <ekioprogram@gmail.com>
2020-06-05 02:30:42 +0900

    Update VulkanTextureView.cpp

--- PlatformLib/Graphics/Vulkan/cpp/VulkanTextureView.cpp
+++ PlatformLib/Graphics/Vulkan/cpp/VulkanTextureView.cpp
@@ -5,31 +5,29 @@ void VulkanTextureView::Create(revDevice* device, const revTexture& texture, con
 {
     this->device = device;
 
-    VkImageViewCreateInfo imageViewCreateInfo = {
-            .sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO,
-            .pNext = nullptr,
-            .viewType = VK_IMAGE_VIEW_TYPE_2D,
-            .components =
-                    {
-                            VK_COMPONENT_SWIZZLE_R, VK_COMPONENT_SWIZZLE_G,
-                            VK_COMPONENT_SWIZZLE_B, VK_COMPONENT_SWIZZLE_A,
-                    },
-            .subresourceRange = {VK_IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 1},
-            .flags = 0,
+    VkImageViewCreateInfo imageViewCreateInfo = {};
+    imageViewCreateInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+    imageViewCreateInfo.pNext = nullptr;
+    imageViewCreateInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+    imageViewCreateInfo.components =
+    {
+            VK_COMPONENT_SWIZZLE_R, VK_COMPONENT_SWIZZLE_G,
+            VK_COMPONENT_SWIZZLE_B, VK_COMPONENT_SWIZZLE_A,
     };
+    imageViewCreateInfo.subresourceRange = { VK_IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 1 };
+    imageViewCreateInfo.flags = 0;
     imageViewCreateInfo.format = ConvertToVKFormat(texture.GetFormat());
     imageViewCreateInfo.image = texture.GetHandle();
+
     VkResult result = vkCreateImageView(device->GetDevice(), &imageViewCreateInfo, nullptr, &resourceView);
     if(result != VK_SUCCESS) {
         NATIVE_LOGE("Vulkan error. File[%s], line[%d]", __FILE__,__LINE__);
         return;
     }
 
-    descriptorImageInfo = {
-            .sampler = sampler.GetHandle(),
-            .imageView = resourceView,
-            .imageLayout = VK_IMAGE_LAYOUT_GENERAL,
-    };
+    descriptorImageInfo.sampler = sampler.GetHandle();
+    descriptorImageInfo.imageView = resourceView;
+    descriptorImageInfo.imageLayout = VK_IMAGE_LAYOUT_GENERAL;
 
     VkWriteDescriptorSet writeDescriptorSet = {};
     writeDescriptorSet.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
