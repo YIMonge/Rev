30ce2fae0d24431cbf0b3f7a95925c25affe88cb
monge <ekioprogram@gmail.com>
2020-06-05 02:30:34 +0900

    Update VulkanSampler.cpp

--- PlatformLib/Graphics/Vulkan/cpp/VulkanSampler.cpp
+++ PlatformLib/Graphics/Vulkan/cpp/VulkanSampler.cpp
@@ -7,23 +7,23 @@ bool VulkanSampler::Create(VulkanDevice* device, const revTexture& texture)
     this->device = device;
     const auto& samplerDesc = texture.GetSamplerDesc();
     // sampler setting
-    VkSamplerCreateInfo samplerCreateInfo = {
-            .sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO,
-            .pNext = nullptr,
-            .magFilter = ConvertToVKFilter(samplerDesc.GetMagFilter()),
-            .minFilter = ConvertToVKFilter(samplerDesc.GetMinFilter()),
-            .mipmapMode = ConvertToVKMipFilterMode(samplerDesc.GetMipFilter()),
-            .addressModeU = ConvertToVKTextureAddressMode(samplerDesc.GetAddressModeU()),
-            .addressModeV = ConvertToVKTextureAddressMode(samplerDesc.GetAddressModeV()),
-            .addressModeW = ConvertToVKTextureAddressMode(samplerDesc.GetAddressModeW()),
-            .mipLodBias = samplerDesc.GetMipLodBias(),
-            .maxAnisotropy = static_cast<f32>(samplerDesc.GetMaxAnisotropy()),
-            .compareOp = ConverToVKComparisonFunc(samplerDesc.GetComparisonFunc()),
-            .minLod = samplerDesc.GetMinLod(),
-            .maxLod = samplerDesc.GetMaxLod(),
-            .borderColor = ConvertToVKBorderColor(samplerDesc.GetBorderColor()),
-            .unnormalizedCoordinates = VK_FALSE,
-    };
+    VkSamplerCreateInfo samplerCreateInfo = {};
+    samplerCreateInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
+    samplerCreateInfo.pNext = nullptr;
+    samplerCreateInfo.magFilter = ConvertToVKFilter(samplerDesc.GetMagFilter());
+    samplerCreateInfo.minFilter = ConvertToVKFilter(samplerDesc.GetMinFilter());
+    samplerCreateInfo.mipmapMode = ConvertToVKMipFilterMode(samplerDesc.GetMipFilter());
+    samplerCreateInfo.addressModeU = ConvertToVKTextureAddressMode(samplerDesc.GetAddressModeU());
+    samplerCreateInfo.addressModeV = ConvertToVKTextureAddressMode(samplerDesc.GetAddressModeV());
+    samplerCreateInfo.addressModeW = ConvertToVKTextureAddressMode(samplerDesc.GetAddressModeW());
+    samplerCreateInfo.mipLodBias = samplerDesc.GetMipLodBias();
+    samplerCreateInfo.maxAnisotropy = static_cast<f32>(samplerDesc.GetMaxAnisotropy());
+    samplerCreateInfo.compareOp = ConverToVKComparisonFunc(samplerDesc.GetComparisonFunc());
+    samplerCreateInfo.minLod = samplerDesc.GetMinLod();
+    samplerCreateInfo.maxLod = samplerDesc.GetMaxLod();
+    samplerCreateInfo.borderColor = ConvertToVKBorderColor(samplerDesc.GetBorderColor());
+    samplerCreateInfo.unnormalizedCoordinates = VK_FALSE;
+    
     VkResult result = vkCreateSampler(device->GetDevice(), &samplerCreateInfo, nullptr, &sampler);
     if(result != VK_SUCCESS) {
         NATIVE_LOGE("Vulkan error. File[%s], line[%d]", __FILE__,__LINE__);
