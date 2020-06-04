b4ff12153db4b792fc588ee5e341f11fc11adec9
monge <ekioprogram@gmail.com>
2020-06-05 02:30:38 +0900

    Update VulkanSwapChain.cpp

--- PlatformLib/Graphics/Vulkan/cpp/VulkanSwapChain.cpp
+++ PlatformLib/Graphics/Vulkan/cpp/VulkanSwapChain.cpp
@@ -25,8 +25,8 @@ bool VulkanSwapChain::Create(VulkanDevice* _device)
 
     uint32 count = 0;
     vkGetPhysicalDeviceSurfaceFormatsKHR(gpu, surface, &count, nullptr);
-    VkSurfaceFormatKHR formats[count];
-    vkGetPhysicalDeviceSurfaceFormatsKHR(gpu, surface, &count, formats);
+    revArray<VkSurfaceFormatKHR> formats(count);
+    vkGetPhysicalDeviceSurfaceFormatsKHR(gpu, surface, &count, formats.data());
 
     uint32 chosenIndex = 0;
     for(; chosenIndex < count; ++chosenIndex){
@@ -42,25 +42,24 @@ bool VulkanSwapChain::Create(VulkanDevice* _device)
 
     uint32 queueFamilyIndex = device->GetQueueFamilyIndex();
 
-    VkSwapchainCreateInfoKHR swapchainCreateInfo{
-            .sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR,
-            .pNext = nullptr,
-            .surface = surface,
-            .minImageCount = capabilities.minImageCount,
-            .imageFormat = ConvertToVKFormat(format),
-            .imageColorSpace = formats[chosenIndex].colorSpace,
-            .imageExtent = capabilities.currentExtent,
-            .imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT,
-            .preTransform = VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR,
-            .imageArrayLayers = 1,
-            .imageSharingMode = VK_SHARING_MODE_EXCLUSIVE,
-            .queueFamilyIndexCount = 1,
-            .pQueueFamilyIndices = &queueFamilyIndex,
-            .compositeAlpha = VK_COMPOSITE_ALPHA_INHERIT_BIT_KHR,
-            .presentMode = VK_PRESENT_MODE_FIFO_KHR,
-            .oldSwapchain = VK_NULL_HANDLE,
-            .clipped = VK_FALSE,
-    };
+    VkSwapchainCreateInfoKHR swapchainCreateInfo = {};
+    swapchainCreateInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+    swapchainCreateInfo.pNext = nullptr;
+    swapchainCreateInfo.surface = surface;
+    swapchainCreateInfo.minImageCount = capabilities.minImageCount;
+    swapchainCreateInfo.imageFormat = ConvertToVKFormat(format);
+    swapchainCreateInfo.imageColorSpace = formats[chosenIndex].colorSpace;
+    swapchainCreateInfo.imageExtent = capabilities.currentExtent;
+    swapchainCreateInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+    swapchainCreateInfo.preTransform = VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR;
+    swapchainCreateInfo.imageArrayLayers = 1;
+    swapchainCreateInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+    swapchainCreateInfo.queueFamilyIndexCount = 1;
+    swapchainCreateInfo.pQueueFamilyIndices = &queueFamilyIndex;
+    swapchainCreateInfo.compositeAlpha = VK_COMPOSITE_ALPHA_INHERIT_BIT_KHR;
+    swapchainCreateInfo.presentMode = VK_PRESENT_MODE_FIFO_KHR;
+    swapchainCreateInfo.oldSwapchain = VK_NULL_HANDLE;
+    swapchainCreateInfo.clipped = VK_FALSE;
 
     const revGraphicsDevice& revDevice = device->GetDevice();
     VkResult result = vkCreateSwapchainKHR(revDevice, &swapchainCreateInfo, nullptr, &swapChain);
@@ -74,11 +73,11 @@ bool VulkanSwapChain::Create(VulkanDevice* _device)
         return false;
     }
     // create fence and semaphore
-    VkFenceCreateInfo fenceCreateInfo = {
-            .sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO,
-            .pNext = nullptr,
-            .flags = 0,
-    };
+    VkFenceCreateInfo fenceCreateInfo = {};
+    fenceCreateInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
+    fenceCreateInfo.pNext = nullptr;
+    fenceCreateInfo.flags = 0;
+    
     result = vkCreateFence(revDevice, &fenceCreateInfo, nullptr, &fence);
     if(result != VK_SUCCESS) {
         NATIVE_LOGE("Vulkan error. File[%s], line[%d]", __FILE__,__LINE__);
@@ -89,11 +88,11 @@ bool VulkanSwapChain::Create(VulkanDevice* _device)
         NATIVE_LOGE("Vulkan error. File[%s], line[%d]", __FILE__,__LINE__);
     }
 
-    VkSemaphoreCreateInfo semaphoreCreateInfo = {
-            .sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO,
-            .pNext = nullptr,
-            .flags = 0,
-    };
+    VkSemaphoreCreateInfo semaphoreCreateInfo = {};
+    semaphoreCreateInfo.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;
+    semaphoreCreateInfo.pNext = nullptr;
+    semaphoreCreateInfo.flags = 0;
+    
     result = vkCreateSemaphore(revDevice, &semaphoreCreateInfo, nullptr, &semaphore);
     if(result != VK_SUCCESS) {
         NATIVE_LOGE("Vulkan error. File[%s], line[%d]", __FILE__,__LINE__);
@@ -135,16 +134,16 @@ bool VulkanSwapChain::Present()
 {
     auto& revDevice = device->GetDevice();
     VkResult result;
-    VkPresentInfoKHR presentInfo = {
-            .sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR,
-            .pNext = nullptr,
-            .swapchainCount = 1,
-            .pSwapchains = &swapChain,
-            .pImageIndices = &frameIndex,
-            .waitSemaphoreCount = 0,
-            .pWaitSemaphores = nullptr,
-            .pResults = &result,
-    };
+    VkPresentInfoKHR presentInfo = {};
+    presentInfo.sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR;
+    presentInfo.pNext = nullptr;
+    presentInfo.swapchainCount = 1;
+    presentInfo.pSwapchains = &swapChain;
+    presentInfo.pImageIndices = &frameIndex;
+    presentInfo.waitSemaphoreCount = 0;
+    presentInfo.pWaitSemaphores = nullptr;
+    presentInfo.pResults = &result;
+
     vkQueuePresentKHR(device->GetQueue(), &presentInfo);
 
     // update buffer index
