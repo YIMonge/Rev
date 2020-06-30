83d4c8f6a59cfb630fa43ee84b68d62d4c40c637
monge <ekioprogram@gmail.com>
2020-06-30 17:59:08 +0900

    create buffers for rendering

--- Rev/cpp/revMeshRenderer.cpp
+++ Rev/cpp/revMeshRenderer.cpp
@@ -10,6 +10,11 @@ revMeshRenderer::~revMeshRenderer()
 {
 }
 
+void revMeshRenderer::SetModel(const revModel& model)
+{
+	SetMeshes(model.GetMeshes());
+}
+
 void revMeshRenderer::SetMeshes(const revArray<revMesh>& meshes)
 {
 	for (uint32 i = 0; i < static_cast<uint32>(meshes.size()); ++i) {
@@ -24,8 +29,39 @@ void revMeshRenderer::SetMesh(uint32 index, const revMesh& mesh)
 	}
 	vertexBuffers[index] = revGraphics::Get().CreateVertexBuffer();
 	if (vertexBuffers[index] == nullptr) {
-		NATIVE_LOGE("faile create vertex buffer. file:%s , line:%s", __FILE__, __LINE__);
+		NATIVE_LOGE("failed create vertex buffer. file:%s , line:%s", __FILE__, __LINE__);
 	}
-	vertexBuffers[index]->Create(mesh.GetVertexData());
+	const auto& vertices = mesh.GetVertexData();
+	const uint32 sizeOfBytes = mesh.GetSizeOfBytes();
+	vertexBuffers[index]->Create(vertices.data(), sizeOfBytes, static_cast<uint32>(vertices.size()) / (sizeOfBytes / sizeof(f32)));
+
+	if (mesh.GetIndexArray().size() > 0) {
+		if (static_cast<uint32>(indexBuffers.size()) <= index) {
+			indexBuffers.resize(index + 1, nullptr);
+		}
+
+		indexBuffers[index] = revGraphics::Get().CreateIndexBuffer();
+		if (indexBuffers[index] == nullptr) {
+			NATIVE_LOGE("failed create index buffer. file:%s , line:%s", __FILE__, __LINE__);
+		}
+		const auto& indicies = mesh.GetIndexArray();
+		indexBuffers[index]->Create(indicies.data(), sizeof(revIndex3), static_cast<uint32>(indicies.size()));
+	}
 }
+
+void revMeshRenderer::SetMaterials(const revArray<revMaterial*>& materials)
+{
+	for (uint32 i = 0; i < static_cast<uint32>(materials.size()); ++i) {
+		SetMaterial(i, materials[i]);
+	}
+}
+
+void revMeshRenderer::SetMaterial(uint32 index, revMaterial* material)
+{
+	if (static_cast<uint32>(materials.size()) <= index) {
+		materials.resize(index + 1, nullptr);
+	}
+	materials[index] = material;
+}
+
 
--- Rev/include/revMeshRenderer.h
+++ Rev/include/revMeshRenderer.h
@@ -1,8 +1,11 @@
 #ifndef __REVMESHRENDERER_H__
 #define __REVMESHRENDERER_H__
 
+#include "revModel.h"
 #include "revMesh.h"
+#include "revMaterial.h"
 #include "revGraphicsBuffer.h"
+#include "revGraphicsCommandList.h"
 
 class revMeshRenderer
 {
@@ -10,11 +13,21 @@ public:
 	revMeshRenderer();
 	~revMeshRenderer();
 
+	void SetModel(const revModel& model);
+
 	void SetMeshes(const revArray<revMesh>& meshes);
 	void SetMesh(uint32 index, const revMesh& mesh);
 
-private:
+	void SetMaterials(const revArray<revMaterial*>& materials);
+	void SetMaterial(uint32 index, revMaterial* material);
+
+	virtual void Draw(revGraphicsCommandList& commandList) = 0;
+
+protected:
 	revArray<revGraphicsBuffer*> vertexBuffers;
+	revArray<revGraphicsBuffer*> indexBuffers;
+	revArray<revMaterial*> materials;
+
 };
 
 #endif
