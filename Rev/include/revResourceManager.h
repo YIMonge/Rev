cba72c4f43fa2c525e0bf4e1494965ae52422998
monge <ekioprogram@gmail.com>
2020-08-01 00:32:07 +0900

    specialization for shader

--- Rev/include/revResourceManager.h
+++ Rev/include/revResourceManager.h
@@ -65,4 +65,14 @@ revMaterial* revResourceManager::Load<revMaterial>(const char* path)
 	return Load<revMaterial, revMaterialLoader>(path);
 }
 
+template<>
+revShader* revResourceManager::Load<revShader>(const char* path)
+{
+#ifdef _USE_DIRECTX12
+	return Load<DX12Shader, revShaderLoader>(path);
+#elif defined(_USE_VULKAN)
+	return Load<VulkanShader, revShaderLoader>(path);
+#endif
+}
+
 #endif
