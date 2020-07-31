cb74fb76e731b26db007450d256cbd23a60c1c3b
monge <ekioprogram@gmail.com>
2020-08-01 00:24:01 +0900

    remove old implement

--- CoreLib/include/revArray.h
+++ CoreLib/include/revArray.h
@@ -8,44 +8,4 @@
 template<typename T>
 using revArray = std::vector<T>;
 
-/*
-template<typename T>
-class revArray
-{
-public:
-	revArray();
-	revArray(uint32 capacity);
-	~revArray();
-
-	void Add(const T& obj);
-	void Remove(uint32 index);
-	void Remove(const T& obj);
-	T& operator [] (int32 i);
-	const T& operator [] (int32 i) const;
-
-	void Resize(uint32 capacity);
-	uint32 Count() const;
-	void Fill(const T& val);
-	void Fill(const T& val, uint32 size);
-
-	ITERATOR_DEF(T, std::forward_iterator_tag);
-	iterator begin()
-	{
-		return iterator(&array[0]);
-	}
-	iterator end()
-	{
-    	return iterator(&array[size - 1]);
-	}	
-
-private:
-	void GrowIfNeeded();
-
-	T* array;
-	uint32 size;
-	uint32 count;
-};
-
-#include "revArray.inl"
-*/
 #endif
--- CoreLib/include/revBitset.h
+++ CoreLib/include/revBitset.h
@@ -7,24 +7,4 @@
 template<int size>
 using revBitset = std::bitset<size>;
 
-/*
-template<typename T, int size>
-class revBitset
-{
-public:
-	revBitset();
-	~revBitset();
-
-	void On(T flag);
-	void Off(T flag);
-	void Reset();
-	bool Test(T flag) const;
-	bool Any() const;
-public:	
-	char buffer[size/CHAR_BIT + 1];
-};
-
-#include "revBitset.inl"
-*/
-
 #endif
