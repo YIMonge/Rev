d65ad56d851791498dd2f42dd5eb2812221a6f69
monge <ekioprogram@gmail.com>
2020-08-01 00:30:20 +0900

    implement shader loader

--- Rev/include/revShaderLoader.h
+++ Rev/include/revShaderLoader.h
@@ -1,9 +1,30 @@
 #ifndef __REVSHADERLOADER_H__
 #define __REVSHADERLOADER_H__
 
-class revShaderLoader
-{
+#include "revShader.h"
+#include "revResourceLoader.h"
+#include "revGraphics.h"
 
+class revShaderLoader : public revResourceLoader<revShader>
+{
+public:
+
+	virtual bool LoadFromFile(const char* path, revShader* resource)
+	{
+		if (resource == nullptr) return false;
+
+		resourcePath = RESOURCE_PATH;
+		resourcePath += path;
+		resource->SetFilePath(path);
+
+		SHADER_TYPE type = SHADER_TYPE::VERTX;
+		if (strstr(path, "_frag") != nullptr) {
+			type = SHADER_TYPE::FRAGMENT;
+		}
+		resource->LoadFromFile(revGraphics::Get().GetDevice(), path, type);
+	
+		return true;
+	}
 };
 
 #endif
