1ebb1972a61c7a8d0ff5d328e1c46572afb97df3
monge <ekioprogram@gmail.com>
2020-09-11 16:28:05 +0900

    use std for queue

--- CoreLib/include/revQueue.h
+++ CoreLib/include/revQueue.h
@@ -1,28 +1,9 @@
 #ifndef __REVQUEUE_H__
 #define __REVQUEUE_H__
 
-#include "revArray.h"
+#include <queue>
 
 template<typename T>
-class revQueue
-{
-public:
-	revQueue();
-	revQueue(uint32 _capacity);
-
-	bool Enqueue(T& obj);
-	T& Dequeue();
-	T& Peek() const;
-	uint32 Count() const;
-	void SetCapacity(uint32 _capacity);
-
-private:
-	uint32 first;
-	uint32 last;
-	uint32 capacity;
-	revArray<T> queue;
-};
-
-#include "revQueue.inl"
+using revQueue = std::queue<T>;
 
 #endif
