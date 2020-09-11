8c815f751838dae333353e476d4e7101766a94f1
monge <ekioprogram@gmail.com>
2020-09-11 16:27:15 +0900

    add fixed bitset and flexible length bitset

--- CoreLib/include/revBitset.h
+++ CoreLib/include/revBitset.h
@@ -2,9 +2,62 @@
 #define __REVBITSET_H__
 
 #include <bitset>
+#include "revTypedef.h"
 
 // TODO: allocator 
 template<int size>
-using revBitset = std::bitset<size>;
+using revFixedBitset = std::bitset<size>;
+
+class revBitset
+{
+public:
+	revBitset(uint32 size)
+	{
+		this->size = size;
+		uint32 index = size / sizeof(uint32);
+		uint32 allocationSize = index == 0 ? 1 : index;
+		buffer = new uint32[size / sizeof(uint32)];
+		memset(buffer, 0, allocationSize * sizeof(uint32));
+	}
+
+	~revBitset()
+	{
+		delete[] buffer;
+	}
+
+	void On(uint32 no)
+	{
+		if (size <= no)
+		{
+			// TODO:assert
+			return;
+		}
+		buffer[no / sizeof(uint32)] |= (1 << no % sizeof(uint32));
+	}
+
+	void Off(uint32 no)
+	{
+		if (size <= no)
+		{
+			// TODO:assert
+			return;
+		}
+		buffer[no / sizeof(uint32)] &= ~(1 << no % sizeof(uint32));
+	}
+
+	bool Test(uint32 no) const 
+	{
+		if (size <= no)
+		{
+			// TODO:assert
+			return false;
+		}
+		return (buffer[no / sizeof(uint32)] & (1 << no % sizeof(uint32))) != 0;
+	}
+
+private:
+	uint32 size;
+	uint32* buffer;
+};
 
 #endif
